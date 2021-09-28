a2e74098c1727b59f218583e8f1a2004b99835ba
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2021-09-28 14:04:29 +0900

    Update decrption.py

--- yolov5-master/decrypt/decrption.py
+++ yolov5-master/decrypt/decrption.py
@@ -7,17 +7,19 @@ script_dir = os.path.dirname(__file__)
 rel_path="/labels/*.txt"
 txt_files = sorted(glob.glob(script_dir + rel_path),key=len)
 #영상파일 읽기
-vidcap = cv2.VideoCapture(script_dir +"/0.mp4")
+vidcap = cv2.VideoCapture(script_dir +"/0.avi")
+# vidcap = cv2.VideoCapture(script_dir +"/0.mp4")
 #노이즈 사진 읽기
-noise_original = cv2.imread('noise.png')
+noise_original = cv2.imread('noise.png') # cd decrypt 후 사용
 
 count_in_list = 0
 frame_count=0
 while vidcap.isOpened():
     #프레임 하나 당 텍스트 파일 하나 꺼내서 읽어 수정 
-    success, frame = vidcap.read()
-    fps, w, h = vidcap.get(cv2.CAP_PROP_FPS), frame.shape[1], frame.shape[0]
+    success, frame = vidcap.read()    
+    #fps, w, h = vidcap.get(cv2.CAP_PROP_FPS), frame.shape[1], frame.shape[0]
     if not success:
+        print("video ends")
         break
     #텍스트 파일 꺼내서 앞에 파일이름 숫자 받음
     file_name = txt_files[count_in_list]
@@ -25,27 +27,37 @@ while vidcap.isOpened():
     print(f"file_num = {file_num}")
     print(f"frame_count = {frame_count}")
     
-    #frame_count가 리스트 위치 count보다 낮음.
+    #frame_count가 리스트 위치 count보다 낮음 = 찾은 얼굴이 없는 상태
     #숫자랑 frame_count랑 불일치하면 
     if file_num != frame_count:
         for i in range(file_num - frame_count):
-            print(f"pass {i}")
+            print(f"pass frame {i} times")
             frame_count+=1
             success, frame = vidcap.read()
+            cv2.imshow("pass",frame)
+            cv2.waitKey(0)      
             
-    print(f"frame_count = {frame_count}")
+        print(f"after pass : frame_count = {frame_count}")
+        
     with open(file_name, "r") as fd:
         for xyxy in fd.readlines():
             coord=list(map(int,xyxy[2:-1].split())) # ex: [236, 146, 369, 329]
+            print(f"coord = {coord}")
             noise = noise_original[coord[1]:coord[3],coord[0]:coord[2]] #노이즈 부분
             encrypted_region = frame[coord[1]:coord[3],coord[0]:coord[2]] #얼굴 부분            
             #encrypted_region에서 noise 9/10 밝기값 뺌
-            noise = noise*0.9
+            noise = noise*0.9                 
+            print(f"encrypted_region = {encrypted_region[0][0]}")
+            print(f"noise = {noise[0][0]}")
             faint_face= encrypted_region-noise                   
+            print(f"faint_face = {faint_face[0][0]}")
+            
             #10배를 곱함
             face = faint_face*10
             frame[coord[1]:coord[3],coord[0]:coord[2]]=face
+    cv2.imshow("after",frame)
+    cv2.waitKey(0)
+    
     count_in_list += 1
     frame_count += 1
-    
 
